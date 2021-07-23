eab0d71fd50dca5362bb4d90e7bf693539b609d0
montenegrop <pablormontenegro1@gmail.com>
2021-07-23 14:47:32 -0300

    changed variables

--- src/state/actions/serviceProvider.ts
+++ src/state/actions/serviceProvider.ts
@@ -1,10 +1,16 @@
 import { createAction } from "@reduxjs/toolkit"
+import { Vendor } from "../../generated/graphql"
 
 export type Service = {
   name: string
   score: number
 }
 
+export type Image = {
+  url: string
+  alt: string
+}
+
 export const SERVICE_PROVIDER_INIT = createAction<{ id: string }>("SERVICE_PROVIDER_INIT")
-export const SERVICE_PROVIDER_SUCCESS = createAction<any>("SERVICE_PROVIDER_SUCCESS")
+export const SERVICE_PROVIDER_SUCCESS = createAction<Vendor>("SERVICE_PROVIDER_SUCCESS")
 export const SERVICE_PROVIDER_ERROR = createAction<string>("SERVICE_PROVIDER_ERROR")
