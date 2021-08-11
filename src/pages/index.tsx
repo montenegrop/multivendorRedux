ba3c2ca179f3fb767c5880b86d09979a171969a1
Pablo Montenegro <42698173+montenegrop@users.noreply.github.com>
2021-08-11 11:04:29 -0300

    Layout (#22)
    * deleted console log
    
    * color por testing
    
    * Laout decorator
    
    * fix, removed divs
    
    * fixes sugeridos para Layout
    
    Co-authored-by: montenegrop <pablormontenegro1@gmail.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,7 @@ import { ServicesTable } from "../components/ServicesTable"
 import { HOMEPAGE_INIT } from "../state/actions/homepage"
 import { SERVICE_PROVIDER_INIT } from "../state/actions/serviceProvider"
 import { RootState } from "../state/reducers"
+import Layout from "./Layout"
 
 export default function Home() {
   const dispatch = useDispatch()
@@ -22,59 +23,45 @@ export default function Home() {
   const banner_image = "https://http2.mlstatic.com/D_NQ_905027-MLA46750656973_072021-OO.webp"
   const avatar_image =
     "https://www.elitesingles.co.uk/wp-content/uploads/sites/59/2019/11/2b_en_articleslide_sm2-350x264.jpg"
-  console.log(services_data, 21)
   return (
-    <div>
-      {/* <section>
-        <figure className="image my_banner_height_200px">
-          <img src="https://bulma.io/images/placeholders/128x128.png" />
-        </figure>
-      </section> */}
-      <div className="columns">
-        <div className="column is-2"></div>
-        <div className="column">
-          <section
-            className="mb-3 my_banner-service_provider is-flex is-align-items-flex-end"
-            style={{ backgroundImage: `url(${banner_image})` }}
-          >
-            <div className="is-flex is-align-items-center mb-3 ml-3">
-              <Avatar image_url={avatar_image}></Avatar>
-              <h2 className="my_1px_black_stroke title is-3 has-text-warning ml-3">
-                Leopoldo Lugones
-              </h2>
-            </div>
-          </section>
-          <section className="mb-3 columns">
-            <div className="column">
-              <h2 className="column_title title has-text-grey-light has-text-weight-light has-background-light mb-0 has-text-centered">
-                CONTACTO
-              </h2>
-              <ContactTable contact_data={{}}></ContactTable>
-            </div>
-            <div className="column">
-              <h2 className="column_title title has-text-grey-light has-text-weight-light has-background-light mb-0 has-text-centered">
-                SERVICIOS
-              </h2>
-              <ServicesTable services_data={services_data}></ServicesTable>
-            </div>
-          </section>
-          <section className="mb-3 columns">
-            <div className="column">
-              <button className="button is-rounded is-primary">Contratar</button>
-            </div>
-            <div className="column"></div>
-          </section>
-
-          <section className="section ml-6">
-            <ServiceButton onClick={onClick}>nombre</ServiceButton>
-          </section>
-          <button onClick={onClick}>nombre del vendor</button>
-          <div className="warning">{name}</div>
-          {loading && <div>loading</div>}
-          <div className="App"></div>
+    <Layout>
+      <section
+        className="mb-3 my_banner-service_provider is-flex is-align-items-flex-end"
+        style={{ backgroundImage: `url(${banner_image})` }}
+      >
+        <div className="is-flex is-align-items-center mb-3 ml-3">
+          <Avatar image_url={avatar_image}></Avatar>
+          <h2 className="my_1px_black_stroke title is-3 has-text-warning ml-3">Leopoldo Lugones</h2>
         </div>
-        <div className="column is-2"></div>
-      </div>
-    </div>
+      </section>
+      <section className="mb-3 columns">
+        <div className="column">
+          <h2 className="column_title title has-text-grey-light has-text-weight-light has-background-light mb-0 has-text-centered">
+            CONTACTO
+          </h2>
+          <ContactTable _contact_data={{}}></ContactTable>
+        </div>
+        <div className="column">
+          <h2 className="column_title title has-text-grey-light has-text-weight-light has-background-light mb-0 has-text-centered">
+            SERVICIOS
+          </h2>
+          <ServicesTable services_data={services_data}></ServicesTable>
+        </div>
+      </section>
+      <section className="mb-3 columns">
+        <div className="column">
+          <button className="button is-rounded is-primary">Contratar</button>
+        </div>
+        <div className="column"></div>
+      </section>
+
+      <section className="section ml-6">
+        <ServiceButton onClick={onClick}>nombre</ServiceButton>
+      </section>
+      <button onClick={onClick}>nombre del vendor</button>
+      <div className="warning">{name}</div>
+      {loading && <div>loading</div>}
+      <div className="App"></div>
+    </Layout>
   )
 }
