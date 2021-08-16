18ca426c142102fe1643a8d5055569737e7080a8
Pablo Montenegro <42698173+montenegrop@users.noreply.github.com>
2021-08-16 18:16:33 -0300

    Next redux homepage2 (#21)
    * Agrego Slider a la home. Saco el border del Navbar
    
    * Nuevo grid y cambios en slider
    
    * cambios de login y svgs
    
    * Grilla: saco placeholders. Detalles y correcciones en estilos. Slider: Cambio breakpoints por los de Bulma. Main: Agrego los .scss faltantates.
    
    * Nuevo navbar con Bulma, pasa a ser componente. Retoques en Slider y Grid
    
    Co-authored-by: Iván Mermelstein <ivan.mermelstein@gmail.com>

---
+++ src/components/CategoryGrid/CategoryGrid.tsx
@@ -0,0 +1,36 @@
+import React, { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { HOMEPAGE_INIT } from "../../state/actions/homepage"
+import { RootState } from "../../state/reducers"
+
+const CategoryGrid = () => {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(HOMEPAGE_INIT())
+  }, [dispatch])
+
+  const homepageCategories = useSelector((state: RootState) => state.homepageCategories.categories)
+
+  return (
+    <div className="grid_container">
+      <div className="columns is-centered is-multiline grid_grid">
+        {homepageCategories &&
+          homepageCategories.map((category) => {
+            return (
+              <div
+                className="column is-one-quarter-desktop is-half-tablet grid_card"
+                key={category.id}
+              >
+                <span className="grid_overlay">
+                  <img className="grid_img" src={category.url} alt={category.alt}></img>
+                  <h3 className="grid_category_name">{category.name}</h3>
+                </span>
+              </div>
+            )
+          })}
+      </div>
+    </div>
+  )
+}
+
+export default CategoryGrid
