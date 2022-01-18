c9883ea181e5c0ca6740197a74f3e3a1e5a4da1e
MartinOfi <martinoficialdegui@hotmail.com>
2022-01-18 16:08:54 -0300

    incorporando headers a las querys

--- src/state/sagas/vendorProducts.ts
+++ src/state/sagas/vendorProducts.ts
@@ -1,4 +1,4 @@
-import { request, gql } from "graphql-request"
+import { gql, GraphQLClient } from "graphql-request"
 import { API_URI } from "../../constants"
 import {
   VENDOR_PRODUCTS_ERROR,
@@ -44,12 +44,19 @@ export default (dispatch) => {
           }
         }
       `
+      const graphQLClient = new GraphQLClient(API_URI, {
+        headers: {
+          authorization:
+            "Bearer JWT eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2NDI0NDM2NjUsImV4cCI6MTY0MjQ0Mzk2NSwidG9rZW4iOiJyWEJFUHJ6RjNQaUwiLCJlbWFpbCI6ImNib2VybzExMUBnbWFpbC5jb20iLCJ0eXBlIjoiYWNjZXNzIiwidXNlcl9pZCI6IlZYTmxjam95TkE9PSIsImlzX3N0YWZmIjp0cnVlfQ.xFLuhHOwdzfmqAz7JGV4tIsl7FYUogRWpe_eeeiFx44",
+        },
+      })
+
       const variables = {
         id: payload.id,
         channel: payload.channel,
       }
       try {
-        const data = await request(API_URI, query, variables)
+        const data = await graphQLClient.request(query, variables)
         dispatch(VENDOR_PRODUCTS_SUCCESS(data.products))
       } catch (error) {
         dispatch(VENDOR_PRODUCTS_ERROR("error de vendor products"))
